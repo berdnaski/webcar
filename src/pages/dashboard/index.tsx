ba60125c8838ddd21134aed0cfcd92f3cfb94cb4
berdnaski <berdnaski1177@gmail.com>
2024-06-18 11:13:42 -0300

    function delete img dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,7 +2,7 @@ import { FiTrash } from "react-icons/fi";
 import { Container } from "../../components/container";
 import { DashboardHeader } from "../../components/panelheader";
 import { useContext, useEffect, useState } from "react";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../services/firebaseConnection";
 import { AuthContext } from "../../contexts/AuthContext";
 
@@ -54,47 +54,54 @@ export function Dashboard() {
                     })
 
                     setCars(listCars);
-                    console.log(listCars);
                 })
         }
 
         loadCars();
     }, [user]);
 
+    async function handleDeleteCar(id: string) {
+        const docRef = doc(db, "cars", id);
+        await deleteDoc(docRef);
+        setCars(cars.filter(car => car.id !== id));
+    }
+
     return (
         <Container>
             <DashboardHeader />
 
             <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-                <section className="w-full bg-white rounded-lg relative">
-                    <button 
-                        onClick={ () => {} }
-                        className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center right-2 top-2 drop-shadow"
-                    >
-                        <FiTrash size={26} color="#000" />
-                    </button>
-                    <img
-                        className="w-full rounded-lg mb-2 max-h-70"
-                        src="https://firebasestorage.googleapis.com/v0/b/webcar-559e8.appspot.com/o/images%2FbCb0wUttYTMPVMEWt51gfZcLJpw1%2F0ff1041c-50a7-42af-818d-e04dfa916cf7?alt=media&token=49d4ef01-172e-4c6f-ab3d-220f4ea1cd64"
-                    />
+                {cars.map(car => (
+                    <section key={car.id} className="w-full bg-white rounded-lg relative">
+                        <button 
+                            onClick={ () => {handleDeleteCar(car.id)} }
+                            className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center right-2 top-2 drop-shadow"
+                        >
+                            <FiTrash size={26} color="#000" />
+                        </button>
+                        <img
+                            className="w-full rounded-lg mb-2 max-h-70"
+                            src={car.images[0].url}
+                        />
 
-                    <p className="font-bold rounded-lg mb-2 max-h-70">NISSAN VERSA</p>
+                        <p className="font-bold rounded-lg mt-1 px-2 mb-2 max-h-70">{car.name}</p>
 
-                    <div className="flex flex-col px-2">
-                        <span className="text-zinc-700">
-                            Ano 2016/2016 | 230.000 km
-                        </span>
-
-                        <strong className="text-black font-bold mt-4">R$ 150.000</strong>
-                    </div>
-
-                    <div className="w-full h-px bg-slate-200 my-2"></div>
-                        <div className="px-2 pb-2">
-                            <span className="text-black">
-                                Campo Grande - MS
+                        <div className="flex flex-col px-2">
+                            <span className="text-zinc-700">
+                                Ano {car.year} | {car.km} km
                             </span>
+
+                            <strong className="text-black font-bold mt-4">R$ {car.price}</strong>
                         </div>
-                </section>
+
+                        <div className="w-full h-px bg-slate-200 my-2"></div>
+                            <div className="px-2 pb-2">
+                                <span className="text-black">
+                                    {car.city}
+                                </span>
+                            </div>
+                    </section>
+                ))}
             </main>
         </Container>
     )
